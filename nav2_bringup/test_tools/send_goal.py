750540544c1a1cd0979fa6795097296a8196a9cd
hello <16340400289@163.com>
2025-07-03 10:31:18 +0800

    增加终点判断

--- nav2_bringup/test_tools/send_goal.py
+++ nav2_bringup/test_tools/send_goal.py
@@ -1,3 +1,5 @@
+import math
+import time
 import typing
 
 import rclpy
@@ -7,6 +9,10 @@ from loguru import logger
 from nav2_msgs.action import NavigateToPose
 from rclpy.action import ActionClient
 from rclpy.node import Node
+from geometry_msgs.msg import TransformStamped
+from tf2_py import TransformException
+from tf2_ros import Buffer, TransformListener
+from tf_transformations import euler_from_quaternion
 
 
 class MyPose:
@@ -32,7 +38,17 @@ class MyPose:
         return temp_pose
 
     def __str__(self):
-        return 'name: %s' % self.name
+        return f'name: {self.name}, x: {self.x}, y: {self.y}, z: {self.z}, qx: {self.qx},  qy: {self.qy}, qz: {self.qz}, qw: {self.qw}'
+
+    def check_offset(self, t: TransformStamped):
+        t_yaw = euler_from_quaternion(
+            [t.transform.rotation.x, t.transform.rotation.y, t.transform.rotation.z, t.transform.rotation.w])[2]
+        self_yaw = euler_from_quaternion([self.qx, self.qy, self.qz, self.qw])[2]
+        dis = ((t.transform.translation.x - self.x) ** 2 + (t.transform.translation.y - self.y) ** 2) ** 0.5
+        yaw_err = abs(t_yaw - self_yaw)
+        if yaw_err > math.pi:
+            yaw_err -= math.pi
+        return yaw_err, dis
 
 
 def read_poses_from_yaml(file_path):
@@ -58,10 +74,29 @@ class FibonacciActionClient(Node):
 
     def __init__(self):
         super().__init__('goal_sender_node')
+        # 初始化TF2监听器
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
         self.goal_sender_client = ActionClient(self, NavigateToPose, '/navigate_to_pose')
         self.pose_list: typing.List[MyPose] = read_poses_from_yaml('poses.yaml')
-        self.pose_index: int = 1
+        self.pose_index: int = 0
+        self.goal_pose: MyPose = self.pose_list[self.pose_index % len(self.pose_list)]
+        self.delay_sec = 10
         self.send_goal()
+
+    def get_transform(self, target_frame: str, source_frame: str) -> TransformStamped:
+        transform_stamped = TransformStamped()
+        try:
+            # 获取TF转换
+            transform_stamped: TransformStamped = self.tf_buffer.lookup_transform(
+                target_frame,
+                source_frame,
+                rclpy.time.Time())
+            return transform_stamped
+        except TransformException as ex:
+            self.get_logger().error(f"获取TF转换失败: {ex}")
+        finally:
+            return transform_stamped
 
     def send_goal(self):
         def _send_goal(_pose: MyPose):
@@ -82,24 +117,39 @@ class FibonacciActionClient(Node):
             self._send_goal_future.add_done_callback(self.goal_response_callback)
 
         self.pose_index += 1
-        goal_pose: MyPose = self.pose_list[self.pose_index % len(self.pose_list)]
-        _send_goal(goal_pose)
+        self.goal_pose: MyPose = self.pose_list[self.pose_index % len(self.pose_list)]
+        logger.info(f'Goal: {self.goal_pose}')
+        self.start_time = time.time()
+        _send_goal(self.goal_pose)
 
     def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected :(')
+            self.delay_to_send_goal()
             return
-
         self.get_logger().info('Goal accepted :)')
-
         self._get_result_future = goal_handle.get_result_async()
         self._get_result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
         result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result))
-        self._timer = self.create_timer()
+        t = self.get_transform('map', 'base_link')
+        yaw_error, dis_error = self.goal_pose.check_offset(t)
+        end_time = time.time()
+        logger.info(
+            f'PointName: {self.goal_pose.name} YawError: {yaw_error}, DisError: {dis_error}m, SpendTime: {end_time - self.start_time}s,Result: {result} ')
+
+        self.delay_to_send_goal()
+
+    def delay_to_send_goal(self):
+        self.timer = self.create_timer(
+            self.delay_sec,
+            self.delayed_callback
+        )
+
+    def delayed_callback(self):
+        self.timer.destroy()
         self.send_goal()
 
     def feedback_callback(self, feedback_msg):
@@ -117,4 +167,5 @@ def main(args=None):
 
 
 if __name__ == '__main__':
+    logger.add('send_goal.log')
     main()
