0e6a143270adbc8e26072734d94992d97cc5822b
hello <16340400289@163.com>
2025-07-04 16:27:10 +0800

    更新地图

--- nav2_bringup/launch/tb3_simulation_launch.py
+++ nav2_bringup/launch/tb3_simulation_launch.py
@@ -87,7 +87,7 @@ def generate_launch_description():
         'map',
         # default_value=os.path.join(
         #     bringup_dir, 'maps', 'turtlebot3_world.yaml'),
-        default_value='/home/hello/agv_ws/src/maps/0701/20250701.yaml',
+        default_value='/home/hello/agv_ws/src/maps/0704/123.yaml',
         description='Full path to map file to load')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
